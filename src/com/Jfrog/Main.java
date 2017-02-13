9d52d169588aeb17a95755871fc360bb56734b76
Shai Ben-Zvi <shaibz@shaibz-mac.jfrog.internal>
2017-02-13 15:47:59 +0200

    add project

---
+++ src/com/Jfrog/Main.java
@@ -0,0 +1,15 @@
+package com.Jfrog;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a Number: ");
+        int someNumber = input.nextInt();
+        System.out.println("The number is: " + someNumber);
+    }
+}
